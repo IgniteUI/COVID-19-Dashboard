0bec794181c09a9a7fc02a6c5e8a57e136b0c12a
Hristo Anastasov <HAnastasov@infragistics.com>
2020-03-17 13:01:43 +0200

    fix routing, add regions, implement caching

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 // base URL for the data files
-const BASE_URL = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series';
+const BASE_URL = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data';
 const TIME_SERIES = 'csse_covid_19_time_series';
+const FILE_NAME = 'time_series_19-covid-';
 const EMPTY_STRING = '';
 
 export enum DATA_SET {
@@ -15,16 +16,32 @@ export enum DATA_SET {
 @Injectable()
 export class RemoteDataService {
     public dataSets = ['Confirmed', 'Recovered', 'Deaths'];
+
+    private data: BehaviorSubject<any[]> = new BehaviorSubject([]);
+    public data$: Observable<any[]> = this.data.asObservable();
+    private cachedData: any[] = [];
+
     constructor() {}
 
     /**
      * Retrieves data from specific file, based on the index passed.
      */
     public getDataSet(index: number): Observable<any> {
-        const baseDataPath = '../../assets/Data/time_series_19-covid-';
+        const baseDataPath = '../assets/Data/';
         const dataSet = this.dataSets[index];
+        const options: RequestInit = {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'no-cors', // no-cors, *cors, same-origin
+          cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
+          // credentials: 'same-origin', // include, *same-origin, omit
+          // headers: {
+          //   'Content-Type': 'application/json'
+          //   // 'Content-Type': 'application/x-www-form-urlencoded',
+          // }
+        };
+
         const data$ = Observable.create(observer => {
-            fetch(`${baseDataPath}${dataSet}.csv`)
+            fetch(`${baseDataPath}/${TIME_SERIES}/${FILE_NAME}${dataSet}.csv`, options)
               .then(response => response.text()) // or text() or blob() etc.
               .then(data => {
                 observer.next(data);
