104c74e2d198bfe7dec7ee01c0a0b8b9c06ac735
Zdravko Kolev <ZKolev@infragistics.com>
2020-03-18 17:04:21 +0200

    Change DS to raw.git and add last updated

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,48 +8,65 @@ const FILE_NAME = 'time_series_19-covid-';
 const EMPTY_STRING = '';
 
 export enum DATA_SET {
-    CONFIRMED = 'Confirmed',
-    RECOVERED = 'Recovered',
-    DEATHS = 'Deaths'
+  CONFIRMED = 'Confirmed',
+  RECOVERED = 'Recovered',
+  DEATHS = 'Deaths'
 }
 
 @Injectable()
 export class RemoteDataService {
-    public dataSets = ['Confirmed', 'Recovered', 'Deaths'];
+  public dataSets = ['Confirmed', 'Recovered', 'Deaths'];
 
-    private data: BehaviorSubject<any[]> = new BehaviorSubject([]);
-    public data$: Observable<any[]> = this.data.asObservable();
-    private cachedData: any[] = [];
+  private data: BehaviorSubject<any[]> = new BehaviorSubject([]);
+  public data$: Observable<any[]> = this.data.asObservable();
+  private cachedData: any[] = [];
 
-    constructor() {}
+  constructor() { }
 
-    /**
-     * Retrieves data from specific file, based on the index passed.
-     */
-    public getDataSet(index: number): Observable<any> {
-        const baseDataPath = 'assets/';
-        const dataSet = this.dataSets[index];
-        const options: RequestInit = {
-          method: 'GET', // *GET, POST, PUT, DELETE, etc.
-          mode: 'no-cors', // no-cors, *cors, same-origin
-          cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
-          // credentials: 'same-origin', // include, *same-origin, omit
-          // headers: {
-          //   'Content-Type': 'application/json'
-          //   // 'Content-Type': 'application/x-www-form-urlencoded',
-          // }
-        };
+  /**
+   * Retrieves data from specific file, based on the index passed.
+   */
+  public getDataSet(index: number): Observable<any> {
+    const baseDataPath = 'assets/';
+    const dataSet = this.dataSets[index];
+    const options: RequestInit = {
+      method: 'GET',    // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors',  // no-cors, *cors, same-origin
+      cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
+    };
+    const data$ = Observable.create(observer => {
+      // tslint:disable-next-line: max-line-length
+      fetch(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-${dataSet}.csv`)
+        .then(response => {
+          return response.text();
+        })
+        .then(data => {
+          observer.next(data);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error('Using offline data; ' + err);
+        });
+    });
 
-        const data$ = Observable.create(observer => {
-            fetch(`${baseDataPath}/${TIME_SERIES}/${FILE_NAME}${dataSet}.csv`, options)
-              .then(response => response.text()) // or text() or blob() etc.
-              .then(data => {
-                observer.next(data);
-                observer.complete();
-              })
-              .catch(err => observer.error(err));
-          });
+    return data$;
+  }
 
-        return data$;
-    }
+  public getLatestCommits(): Observable<any> {
+    const resultData$ = Observable.create(observer => {
+      fetch(`https://api.github.com/repos/CSSEGISandData/COVID-19/commits`)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          observer.next(data);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
+
+    return resultData$;
+  }
 }
