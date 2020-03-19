00cfd0104e9f4216dd62ecfce458f7a9af657e8b
Zdravko Kolev <ZKolev@infragistics.com>
2020-03-19 16:07:51 +0200

    Fix the calculations problem

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -118,9 +118,23 @@ export class MainComponent implements OnInit, OnDestroy {
     let columns = [];
     let day: string = null;
     const cases: Map<string, number> = new Map();
+    let transformedCases: Map<string, number> = new Map();
     let totalRecoveredCases: Map<string, number> = new Map();
     const casesOtherLocations: Map<string, number> = new Map();
     const casesChina: Map<string, number> = new Map();
+    const firstRecordedDate = new Date('01/21/2020').toDateString();
+
+    /*
+      This is for the current cases count on 21st
+      There are 332 total cases for 21st, which means that for 22nd we will have 555 - 332 = 223
+
+      Total cases                 = 332
+      Total cases China           = 326
+      Total cases other locations = 6
+    */
+    cases.set(firstRecordedDate, 332);
+    casesChina.set(firstRecordedDate, 326);
+    casesOtherLocations.set(firstRecordedDate, 6);
 
     // tslint:disable-next-line: prefer-for-of
     for (let rowIdx = 0; rowIdx < csvData.length; rowIdx++) {
@@ -128,55 +142,66 @@ export class MainComponent implements OnInit, OnDestroy {
 
       for (let columnIdx = 4; columnIdx <= columns.length - 1; columnIdx++) {
         if (rowIdx === 0) {
-          day = new Date(columns[columnIdx]).toUTCString();
+          day = new Date(columns[columnIdx]).toDateString();
+
           cases.set(day, 0);
           casesChina.set(day, 0);
           casesOtherLocations.set(day, 0);
         } else {
-          const compound = cases.get(new Date(csvData[0].split(',')[columnIdx]).toUTCString()) +
-            parseInt(columns[columnIdx], 10);
-          cases.set(new Date(csvData[0].split(',')[columnIdx]).toUTCString(), compound);
+          const mapKey = new Date(csvData[0].split(',')[columnIdx]).toDateString();
+          const compound = cases.get(mapKey) + parseInt(columns[columnIdx], 10);
+          cases.set(mapKey, compound);
 
           if (columns[1] === 'China') {
-            const compoundChina = casesChina.get(new Date(csvData[0].split(',')[columnIdx]).toUTCString()) +
-              parseInt(columns[columnIdx], 10);
-            casesChina.set(new Date(csvData[0].split(',')[columnIdx]).toUTCString(), compoundChina);
+            const compoundChina = casesChina.get(mapKey) + parseInt(columns[columnIdx], 10);
+            casesChina.set(mapKey, compoundChina);
           } else {
-            const compoundOther = casesOtherLocations.get(new Date(csvData[0].split(',')[columnIdx]).toUTCString()) +
-              parseInt(columns[columnIdx], 10);
-            casesOtherLocations.set(new Date(csvData[0].split(',')[columnIdx]).toUTCString(), compoundOther);
+            const compoundOther = casesOtherLocations.get(mapKey) + parseInt(columns[columnIdx], 10);
+            casesOtherLocations.set(mapKey, compoundOther);
           }
         }
       }
     }
 
-    // Persist the total recovered cases before transforming the 'cases' map
+    // Persist the total recovered cases (on the second call of fillData) before transforming the 'cases' map
     // This number will be different for both Confirmed and Recovered data sources
     // This is why we are going to use it only from the second request
     totalRecoveredCases = new Map(cases);
+    transformedCases = new Map(cases);
 
     // Calculate daily difference and transform map
-    for (let index = 0; index < cases.size; index++) {
+    for (let index = 0; index <= cases.size; index++) {
 
       // Calculate only daily difference.
       let newCasesCount = 0;
-      const currentElementKey = Array.from(cases.keys())[index];
+      let currentElementKey = Array.from(cases.keys())[index];
       const currentElementValue =  cases.get(currentElementKey);
 
       const nextElementKey = Array.from(cases.keys())[index + 1];
       const nextElementValue =  cases.get(nextElementKey);
 
+
+      // Set the actual date to be the date for which we calculate the daily change
+      const actualDailyChangeDate = new Date(currentElementKey);
+      actualDailyChangeDate.setDate(actualDailyChangeDate.getDate() + 1);
+      currentElementKey = actualDailyChangeDate.toDateString();
+
+      // For each of the next days, add the value as it was yesterday
       if (currentElementValue < nextElementValue) {
         newCasesCount = nextElementValue - currentElementValue;
 
-        cases.set(currentElementKey, newCasesCount);
+        transformedCases.set(currentElementKey, newCasesCount);
       } else {
-        cases.set(currentElementKey, nextElementValue);
+        transformedCases.set(currentElementKey, nextElementValue);
       }
     }
 
+    // Fill the first day 21st of January and use it as a starting point
+    // The daily difference for 21st of January is 50
+    transformedCases.set(firstRecordedDate, 50);
+
     // [0] Daily difference, [1] Other locations, [2] China, [3] Total recovered cases
-    return [cases, casesOtherLocations, casesChina, totalRecoveredCases];
+    return [transformedCases, casesOtherLocations, casesChina, totalRecoveredCases];
   }
 
   public ngOnDestroy() {
