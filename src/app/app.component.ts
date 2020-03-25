ca493bee1cdf6a615403d4bc1c1a059ed766d44e
MPopov <desig9stein@gmail.com>
2020-03-25 17:15:16 +0200

    splash screen initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, ViewChild} from '@angular/core';
+import {AfterContentInit, Component, ViewChild} from '@angular/core';
 import {MainComponent} from './main/main.component';
+import {SplashscreenComponent} from './splashscreen/splashscreen.component';
 
 
 @Component({
@@ -8,13 +9,19 @@ import {MainComponent} from './main/main.component';
   styleUrls: ['./app.component.scss'],
   host: {class: 'app'}
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   public darkTheme = true;
+  public ssVisability = '';
   @ViewChild('main', {static: true}) public main: MainComponent;
+  @ViewChild('splash-screen', {static: true}) public splash: SplashscreenComponent;
   constructor() {}
 
   public toggleTheme() {
     this.darkTheme = !this.darkTheme;
     this.main.map.changeMap(this.darkTheme);
+  }
+
+  ngAfterContentInit() {
+    this.ssVisability = 'splash-screen--hidden';
   }
 }
