415372541084982ac792467bb5337a2aaaf04e40
Aleksandyr <aleksandyr.kamenov@gmail.com>
2020-11-12 15:37:00 +0200

    update to 11

--- src/app/list-cases/list-cases.component.spec.ts
+++ src/app/list-cases/list-cases.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ListCasesComponent } from './list-cases.component';
 
@@ -6,7 +6,7 @@ describe('ListCasesComponent', () => {
   let component: ListCasesComponent;
   let fixture: ComponentFixture<ListCasesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ListCasesComponent ]
     })
